d49d8edec52166f93df98568c6371b3e2b8bc258
Vipin Mishra <thevipinmishra@gmail.com>
2024-12-15 09:58:29 +0530

    web: remove content collections and react-hook-form

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import { withContentCollections } from "@content-collections/next";
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
 };
 
-export default withContentCollections(config);
+export default config;
