be211aa15831ab79bdedf125e1acea3529351b12
Vipin Mishra <thevipinmishra@gmail.com>
2025-01-22 10:22:05 +0530

    re-export utility components

---
+++ src/components/utils/index.tsx
@@ -0,0 +1,9 @@
+import { LocaleProvider } from '@ark-ui/react/locale'
+import { Highlight } from '@ark-ui/react/highlight'
+import {Format} from '@ark-ui/react/format'
+import { FocusTrap } from "@ark-ui/react/focus-trap";
+
+
+
+
+export { LocaleProvider, Highlight, Format, FocusTrap }
